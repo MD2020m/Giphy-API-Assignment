60ff3e57a994f85f6504ebc0d5cf4f257d11f979
MD2020m <dmarshall2019@gmail.com>
2025-10-04 18:15:35 -0500

    Split fetchAndDisplayGifs() into fetchGif() and displayGifs() functions for improved modularity and separation of logic

--- js/script.js
+++ js/script.js
@@ -13,11 +13,13 @@ searchBtn.addEventListener("click", async function() {
         searchStr = "Puppy";
     }
     // Call fetchGif function with searchGif as argument
-    fetchAndDisplayGifs(searchStr);    
+    let gifsJSON = await fetchGifs(searchStr); //Use await so fetchGifs() will perform asynchronously
+    // Call displayGifs to output gifs on webpage
+    displayGifs(gifsJSON); 
 })
 
-// Define fetchGif as async function
-async function fetchAndDisplayGifs(searchString) {
+// Define fetchGifs as async function
+async function fetchGifs(searchString) {
     // Format string containing Giphy API URL including query text from search bar input
     let APIurl = `https://api.giphy.com/v1/gifs/search?api_key=TqpiFQIS5QLzlroUSsLz2vjTZDOFmgzF&q=${searchString}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
     
@@ -27,12 +29,16 @@ async function fetchAndDisplayGifs(searchString) {
     const data = await response.json();
     // Pull only the image data out of the JSON object
     const imgs = data.data;
+    return imgs;
+}
 
-    // Remove previous gifs from gif container on each new search
+// Define displayGifs() to display gifs from a JSON returned from Giphy API
+function displayGifs(gifsJSON) {
+    // Clear previous gifs from output container before displaying gifs from newest search
     gifContainer.innerHTML = "";
 
-    // Add each image returned from API call to the output gif container
-    imgs.forEach(function(gif) {
-        gifContainer.innerHTML += `<img src=${gif.images.original.url} class = "col-3 mb-3">`;
+    // Display gifs returned from API call
+    gifsJSON.forEach(function(gif) {
+        gifContainer.innerHTML += `<img src=${gif.images.original.url} class = "col-3 mb-3">`
     })
 }
